88184b09f04bc738b9b32d8e507edb9e0455ff6c
unknown <kemayerar@gmai.com>
2024-10-28 20:18:23 -0300

    updated activity 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("what is your grade? ");
+        string answer = Console.ReadLine();
+        int number = int.Parse(answer);
+        string letter;
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >=70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine(letter);
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You sloth! study harder next time");
+        }
     }
 }
