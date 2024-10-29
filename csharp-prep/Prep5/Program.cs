40f7c6574fa985f31e5c3d043bf14d64d0daf5a4
unknown <kemayerar@gmai.com>
2024-10-29 14:39:18 -0300

    exercise 5 done

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Welcomer();
+
+        string name = NameAsker();
+        int number = NumberAsker();
+        string squaredNumber = SquareMaker(number).ToString();
+
+        Displayer(name, squaredNumber);
+
     }
+    static void Welcomer()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string NameAsker()
+    {
+        Console.WriteLine("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int NumberAsker()
+    {
+        Console.WriteLine("Please insert a random number ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareMaker(int number2)
+    {
+        int square = number2 * number2;
+        string newSquare = square.ToString();
+        return square;
+    }
+    static void Displayer(string name, string square)
+    {
+        Console.WriteLine($"your new username is {name}{square}");
+    }
+
 }
