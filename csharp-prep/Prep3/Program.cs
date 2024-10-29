406eee0f3c070b911f2861983b496c64ae206be5
unknown <kemayerar@gmai.com>
2024-10-28 21:21:07 -0300

    Exercise 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        Console.WriteLine("Random number generated!");
+        int guess = 0;
+        while (number != guess)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+                Console.WriteLine();
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Congrats! you escaped the matrix");
+                Console.WriteLine();
+            }
+        } 
+
     }
 }
